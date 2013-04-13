54caf755e2a8e80719e1ae893a2335f3bc578bda
stonexing <stonexing5@gmail.com>
2013-04-13 17:08:15 +0800

    Initial Commit

---
+++ SpecialTask/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  SpecialTaskAppController.h
+//  SpecialTask
+//
+//  Created by Xingyan on 13-4-13.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ SpecialTask/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  SpecialTaskAppController.h
+//  SpecialTask
+//
+//  Created by Xingyan on 13-4-13.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
